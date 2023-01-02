89d218170142891233e7461da7cbda7599e41aea
Lymeng Naret <lymengnaret@Lymengs-MacBook-Air.local>
2023-01-01 20:29:03 -0800

    full-screen capture

--- src/main.py
+++ src/main.py
@@ -1,43 +1,43 @@
 import numpy as np
 import cv2 as cv
 from PIL import ImageGrab
-# import pyautogui
+import pygetwindow
+import pyautogui
 import sys
 import os
 import platform
+#
+#
+def main():
+    
+    # The os.chdir changes the current working directory to the specified directory. 
+    # The os.path.dirname returns the directory component of a file path.
+    # the os.path.abspath returns the absolute file path of a file.
+    # The __file__ variable is a built-in variable that holds the file path of the current script.
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# The os.chdir changes the current working directory to the specified directory. 
-# The os.path.dirname returns the directory component of a file path.
-# the os.path.abspath returns the absolute file path of a file.
-# The __file__ variable is a built-in variable that holds the file path of the current script.
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    if sys.platform == "darwin": # allow in Security & Privacy to enable
+        title = pygetwindow.getAllTitles()
+        print(title)
+        while(True):
+            screenshot  = ImageGrab.grab(
+                # all_screens=True,
+                bbox=(0, 0, 200, 200),
+                )
+            screenshot = np.array(screenshot)
+            # BGR -> RGB
+            screenshot = cv.cvtColor(screenshot, cv.COLOR_BGR2RGB)   
 
-def list_window_names():
-    window_names = []
-
-    if sys.platform == "darwin":
-        from AppKit import NSWorkspace
-        workspace = NSWorkspace.sharedWorkspace()
-        for window in workspace.runningApplications():
-            # if window.isActive():
-            window_names.append(window.localizedName())
+            cv.imshow("img", screenshot)
+            if cv.waitKey(1) == ord('q'):
+                cv.destroyAllWindows()
+                break 
+    # TODO
     elif sys.platform == "win32":
-        (active_app_name, windowTitle) = _getActiveInfo_Win32() 
-
-    return window_names
-
-print(list_window_names())
-
-while(True):
-    screenshot  = ImageGrab.grab()
-    screenshot = np.array(screenshot)
-    # BGR -> RGB
-    screenshot = cv.cvtColor(screenshot, cv.COLOR_BGR2RGB)   
-
-    cv.imshow("img", screenshot)
-
-    if cv.waitKey(1) == ord('q'):
-        cv.destroyAllWindows()
-        break
-
+        print()
+        
+#
+#
+if __name__ == '__main__':
+    main()
 
