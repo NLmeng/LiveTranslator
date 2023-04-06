795d5c751f4ffc51d9ae48753c55acbb16493a2a
Lymeng Naret <lymengnaret@yahoo.com>
2023-04-06 16:34:34 -0700

    structure for openai response

--- src/openai/main.py
+++ src/openai/main.py
@@ -1,18 +1,29 @@
 import os
-
 import openai
 
-openai.api_key_path = "key.txt"
-# openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="A neutron star is the collapsed core of a massive supergiant star, which had a total mass of between 10 and 25 solar masses, possibly more if the star was especially metal-rich.[1] Neutron stars are the smallest and densest stellar objects, excluding black holes and hypothetical white holes, quark stars, and strange stars.[2] Neutron stars have a radius on the order of 10 kilometres (6.2 mi) and a mass of about 1.4 solar masses.[3] They result from the supernova explosion of a massive star, combined with gravitational collapse, that compresses the core past white dwarf star density to that of atomic nuclei.\n\nTl;dr",
-  temperature=0.7,
-  max_tokens=60,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=1
-)
+def generate_response(model, prompt, api_key_path="key.txt"):
+    """
+    Generates a response from the OpenAI API using the specified model and prompt
+    :param model: The model to use for generating the response
+    :type model: str, one of "gpt-3.5-turbo", "gpt-3.5-turbo-0301", "text-davinci-003", "text-davinci-002", ...
+    :param prompt: The text prompt to generate the response from
+    :type prompt: str
+    :return: The generated text from the response
+    """
+    # Set the API key path
+    openai.api_key_path = api_key_path
 
-print(response)
+    # Call the OpenAI API to generate a response
+    response = openai.Completion.create(
+        model=model,  # The model to use for generating the response
+        prompt=prompt,  # The text prompt to generate the response from
+        temperature=0.7,  # Controls randomness: higher value -> more random, lower value -> more focused
+        max_tokens=60,  # Maximum number of tokens (words) in the generated response
+        top_p=1.0,  # Controls diversity: higher value -> more diverse, lower value -> less diverse
+        frequency_penalty=0.0,  # Controls token frequency: lower value -> penalizes frequent tokens, higher value -> allows frequent tokens
+        presence_penalty=1  # Controls token presence: lower value -> penalizes new tokens, higher value -> allows new tokens
+    )
+
+    # Return the generated text from the response, removing any leading/trailing whitespace
+    return response.choices[0].text.strip()
