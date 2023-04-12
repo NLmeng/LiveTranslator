81ac0852b7c1b5f455d5c5d1f10142a77809179b
Lymeng Naret <lymengnaret@yahoo.com>
2023-04-12 01:56:40 -0700

    choose window

--- src/ocr/ocr.py
+++ src/ocr/ocr.py
@@ -2,14 +2,16 @@ import numpy as np
 import sys
 import os
 import cv2 as cv
+import pygetwindow as gw
+import Quartz
 from PIL import ImageGrab, Image
 from pytesseract import pytesseract
-
+from deep_translator import GoogleTranslator
 
 class OCR:
     def __init__(self):
-        self.change_working_directory()
         self.logfilename = "text_output.txt"
+        self.change_working_directory()
 
     def change_working_directory(self):
         """
@@ -21,21 +23,49 @@ class OCR:
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
         open(self.logfilename , "w").close()
 
+    def choose_window(self):
+        # List all open windows
+        windows = []
+        for window in Quartz.CGWindowListCopyWindowInfo(Quartz.kCGWindowListOptionOnScreenOnly, Quartz.kCGNullWindowID):
+            if window.get('kCGWindowOwnerName') and window.get('kCGWindowName'):
+                windows.append(window)
+        
+        print("Available windows:")
+        for i, window in enumerate(windows):
+            print(f"{i + 1}. {window.get('kCGWindowOwnerName')} - {window.get('kCGWindowName')}")
+
+        # Prompt the user to choose a window
+        while True:
+            try:
+                choice = int(input("Enter the number of the window you want to capture: "))
+                if 1 <= choice <= len(windows):
+                    break
+                else:
+                    print("Invalid choice. Please try again.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+        return windows[choice - 1]
+
     def screen_capture(self):
         #TODO: print message: "allow in Security & Privacy to enable" if not working / add to readme
+        target_window = self.choose_window()
         if sys.platform == "darwin": 
+            
+            x, y, w, h = int(target_window['kCGWindowBounds']['X']), int(target_window['kCGWindowBounds']['Y']), int(target_window['kCGWindowBounds']['Width']), int(target_window['kCGWindowBounds']['Height'])
+            screenshot = ImageGrab.grab(bbox=(x, y, x + w, y + h))
+            screenshot = np.array(screenshot)
+            screenshot = cv.cvtColor(screenshot, cv.COLOR_BGR2RGB)
+            self.extractTexts(screenshot)
             while True:
-                screenshot = ImageGrab.grab()
-                screenshot = np.array(screenshot)
-                screenshot = cv.cvtColor(screenshot, cv.COLOR_BGR2RGB)
-                self.extractTexts(screenshot)
                 cv.imshow("img", screenshot)
+
                 if cv.waitKey(1) == ord('q'):
                     cv.destroyAllWindows()
                     break
         elif sys.platform == "win32":
         # TODO
-            print()  
+            print()
 
     def extractTexts(self, img):  
         """
@@ -59,7 +89,11 @@ class OCR:
 
             cropped = img[y:y + h, x:x + w]
             text = pytesseract.image_to_string(cropped)
+            translated_text = GoogleTranslator(source='auto', target='en').translate(text)
 
             file = open(self.logfilename , "w")
-            file.write(text + "\n")
+            file.write(translated_text + "\n")
             file.close
+
+            # Draw translated text on the image
+            img = cv.putText(img, translated_text, (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
