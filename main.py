a27ef5b70682eed0ac8adda7d1088a807b519d91
Lymeng Naret <lymengnaret@yahoo.com>
2023-04-12 00:01:33 -0700

    clean main and refactor ocr

--- main.py
+++ main.py
@@ -1,41 +1,5 @@
-import sys
-import os
-import platform
-import threading
-import queue
-from src.ocr.extractTexts import extractTexts
-from ui.UI import startGUI
-from src.openai.main import generate_response
-from src.clipboard.ClipboardListener import ClipboardListener
-from src.office.onenote import write_to_onenote
-from src.office.token import acquire_token_func
+from src.ocr.ocr import OCR
 
-# Create a queue to store the copied content
-copied_content_queue = queue.Queue()
 
-def start_listening_with_queue(copied_content_queue):
-    """
-    Calls start_listening() and puts the new copied content in the queue
-    :param copied_content_queue: The queue to store the copied content
-    :type copied_content_queue: queue.Queue
-    """
-    clipboard_listener = ClipboardListener()
-    for copied_content in clipboard_listener.start_listening():
-        copied_content_queue.put(copied_content)
-
-def process_copied_content():
-    """
-    Processes the copied content in the queue by generating a prompt and potentially a response
-    """
-    while True:
-        if not copied_content_queue.empty():
-            prompt = copied_content_queue.get()
-            response = generate_response("gpt-3.5-turbo", prompt)
-
-if __name__ == "__main__":
-    # Start the start_listening_with_queue function in a separate thread
-    listener_thread = threading.Thread(target=start_listening_with_queue, args=(copied_content_queue,), daemon=True)
-    listener_thread.start()
-
-    # Run the process_copied_content function in the main thread
-    process_copied_content()
+if __name__ == '__main__':
+    OCR().screen_capture()
--- src/ocr/ocr.py
+++ src/ocr/ocr.py
@@ -1,4 +1,65 @@
+import numpy as np
+import sys
+import os
+import cv2 as cv
+from PIL import ImageGrab, Image
+from pytesseract import pytesseract
 
 
-#
-#
+class OCR:
+    def __init__(self):
+        self.change_working_directory()
+        self.logfilename = "text_output.txt"
+
+    def change_working_directory(self):
+        """
+        The os.chdir changes the current working directory to the specified directory. 
+        The os.path.dirname returns the directory component of a file path.
+        the os.path.abspath returns the absolute file path of a file.
+        The __file__ variable is a built-in variable that holds the file path of the current script.
+        """
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        open(self.logfilename , "w").close()
+
+    def screen_capture(self):
+        #TODO: print message: "allow in Security & Privacy to enable" if not working / add to readme
+        if sys.platform == "darwin": 
+            while True:
+                screenshot = ImageGrab.grab()
+                screenshot = np.array(screenshot)
+                screenshot = cv.cvtColor(screenshot, cv.COLOR_BGR2RGB)
+                self.extractTexts(screenshot)
+                cv.imshow("img", screenshot)
+                if cv.waitKey(1) == ord('q'):
+                    cv.destroyAllWindows()
+                    break
+        elif sys.platform == "win32":
+        # TODO
+            print()  
+
+    def extractTexts(self, img):  
+        """
+        use pytesseract
+        processes (> greyscale > binary > dilute > contours) and extracts texts from img
+        locate area of white pixels > draw boundary > crop > extract
+        """
+        greyed_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        bin0, bin1 = cv.threshold(
+            greyed_img, 0, 255, cv.THRESH_OTSU | cv.THRESH_BINARY_INV)
+        dilation = cv.dilate(
+            bin1,
+            cv.getStructuringElement(cv.MORPH_RECT, (6, 6)),
+            iterations=3
+        )
+        contours, hierarchy = cv.findContours(
+            dilation, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+        for curr in contours:
+            x, y, w, h = cv.boundingRect(curr)
+
+            cropped = img[y:y + h, x:x + w]
+            text = pytesseract.image_to_string(cropped)
+
+            file = open(self.logfilename , "w")
+            file.write(text + "\n")
+            file.close
