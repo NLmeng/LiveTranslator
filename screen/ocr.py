93d54d885ecf318713eeb0039574a9a9631a6f35
Lymeng <60766732+NLmeng@users.noreply.github.com>
2024-05-06 11:53:48 -0700

    Segmenting and Postprocess (#29)
    * Aggregate by line_num
    
    * segment image into blocks and use psm 3
    
    * correctly overlays translated texts

--- screen/ocr.py
+++ screen/ocr.py
@@ -1,50 +1,72 @@
 import re
-
+import cv2
+import numpy as np
 import pytesseract
 from pytesseract import Output
-
 
 def get_lang_charset(lang_code):
     """Returns a regular expression pattern for characters typical of the given language."""
     charsets = {
-        'eng': r'[a-zA-Z0-9\s,.!?;:-]', # includes kana and kanji
-        'jpn': r'[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\s,.!?;:-]',
+        'eng': r'[a-zA-Z0-9\s,.!?;:-]',
+        'jpn': r'[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\s,.!?;:-]',  # Includes Kana and Kanji
         'fra': r'[a-zA-Z0-9\s,.!?;:\-àâçéèêëîïôûùüÿœ]',
     }
     return charsets.get(lang_code, r'[a-zA-Z0-9\s,.!?;:-]')
 
-
-def extract_text_and_boxes(image, lang='eng', psm=6):
-    config = f'--psm {psm} --oem 3 -l {lang}'
-    data = pytesseract.image_to_data(
-        image, config=config, output_type=Output.DICT)
+def extract_text_from_blocks(image, bounding_boxes, lang='eng'):
+    """Extracts text from each block based on bounding boxes using pytesseract."""
     text_box_pairs = []
-    current_block_num = None
-    current_text = ''
-    current_box = [0, 0, 0, 0]
     language_pattern = get_lang_charset(lang)
 
-    for i in range(len(data['text'])):
-        if data['text'][i].strip() != '':
-            filtered_text = ''.join(re.findall(
-                language_pattern, data['text'][i]))
-            if filtered_text:
-                if data['block_num'][i] != current_block_num:
-                    if current_text:
-                        text_box_pairs.append(
-                            (current_text, tuple(current_box)))
-                    current_block_num = data['block_num'][i]
-                    current_text = filtered_text
-                    current_box = [data['left'][i], data['top']
-                                   [i], data['width'][i], data['height'][i]]
-                else:
-                    current_text += ' ' + filtered_text
-                    current_box[2] = max(
-                        current_box[2], data['left'][i] + data['width'][i] - current_box[0])
-                    current_box[3] = max(
-                        current_box[3], data['top'][i] + data['height'][i] - current_box[1])
+    for box in bounding_boxes:
+        x, y, w, h = box
+        roi = image[y:y+h, x:x+w]
 
-    if current_text:
-        text_box_pairs.append((current_text, tuple(current_box)))
+        data = pytesseract.image_to_data(roi, config=f'--oem 3 -l {lang}', output_type=Output.DICT)
+
+        grouped_text = {}
+        for i in range(len(data['text'])):
+            if data['text'][i].strip() != '':
+                filtered_text = ''.join(re.findall(language_pattern, data['text'][i]))
+                if filtered_text:
+                    key = (data['block_num'][i], data['line_num'][i])
+                    left, top, width, height = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
+                    box_in_roi = (left, top, width, height)
+                    global_box = (x + box_in_roi[0], y + box_in_roi[1], box_in_roi[2], box_in_roi[3])
+
+                    if key not in grouped_text:
+                        grouped_text[key] = {'text': filtered_text, 'box': list(global_box)}
+                    else:
+                        grouped_text[key]['text'] += ' ' + filtered_text
+                        grouped_text[key]['box'][2] = max(grouped_text[key]['box'][2], global_box[0] + global_box[2] - grouped_text[key]['box'][0])
+                        grouped_text[key]['box'][3] = max(grouped_text[key]['box'][3], global_box[1] + global_box[3] - grouped_text[key]['box'][1])
+
+        for key, value in grouped_text.items():
+            text_box_pairs.append((value['text'], tuple(value['box'])))
 
     return text_box_pairs
+
+def segment_image(image):
+    """
+        Segments the image into blocks of texts using contour detection.
+        - applies grayscale
+        - applies adaptive thresholding
+        - dilates the image
+        - finds contours
+        - filters contours by size
+        - sorts bounding boxes by y-coordinate
+    """
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 5))
+    dilated = cv2.dilate(binary, kernel, iterations=2)
+
+    contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    bounding_boxes = [cv2.boundingRect(contour) for contour in contours if cv2.contourArea(contour) > 500]
+
+    bounding_boxes = sorted(bounding_boxes, key=lambda x: (x[1], x[0]))
+
+    return bounding_boxes
--- src/FrameTranslator.py
+++ src/FrameTranslator.py
@@ -1,13 +1,10 @@
 import threading
 from queue import Queue
-
 import cv2
-
 from preprocess.Preprocessor import Preprocessor
-from screen.controller import get_image_from_path, put_text_on_frame
-from screen.ocr import extract_text_and_boxes
+from screen.controller import get_image_from_path, put_text_on_frame, draw_bounding_boxes
 from translate.translator import translate_text
-
+from screen.ocr import segment_image, extract_text_from_blocks
 
 class FrameTranslator:
     def __init__(self, img_path, source_lang='', target_lang='eng', print_text=False, print_boxes=False):
@@ -21,6 +18,7 @@ class FrameTranslator:
         if not source_lang or source_lang == '':
             source_lang = preprocessor.get_lang_code()
         self.frame = preprocessor.process_image()
+        self.source_lang = source_lang
         self.translation_queue = Queue()
         self.threads = []
         self.lang_code_map = {
@@ -35,30 +33,33 @@ class FrameTranslator:
             if item is None:
                 break
             text, box = item
-            target_lang_code = self.lang_code_map.get(
-                self.target_lang, self.target_lang)
+            target_lang_code = self.lang_code_map.get(self.target_lang, self.target_lang)
             translated_text = translate_text(text, target_lang_code)
-            put_text_on_frame(self.frame, translated_text,
-                              box, draw_box=self.print_boxes)
+            put_text_on_frame(self.frame, translated_text, box, draw_box=self.print_boxes)
             self.translation_queue.task_done()
 
     def start_translation_process(self):
-        text_box_pairs = extract_text_and_boxes(self.frame, self.source_lang)
-
+        bounding_boxes = segment_image(self.frame)
+        
+        # self.frame = draw_bounding_boxes(self.frame, bounding_boxes)
+        # cv2.imwrite("segmented_frame.jpg", self.frame)
+        
+        text_box_pairs = extract_text_from_blocks(self.frame, bounding_boxes, lang=self.source_lang)
+        
         for _ in range(4):
             thread = threading.Thread(target=self.worker)
             thread.start()
             self.threads.append(thread)
-
+            
         for text, box in text_box_pairs:
             if self.print_text:
-                print(f"Text: {text}")
+                print(f"Text: {text} at {box}")
             self.translation_queue.put((text, box))
-
+            
         for _ in range(4):
             self.translation_queue.put(None)
-
+            
         for thread in self.threads:
             thread.join()
-
+            
         cv2.imwrite("translated_frame.jpg", self.frame)
